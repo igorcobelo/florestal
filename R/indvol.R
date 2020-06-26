efee0aa085804ea258cfb58c91b8149912b87217
Igor Cobelo <igorcf@hotmail.com>
2020-06-25 23:21:25 -0300

    nv

--- R/indvol.R
+++ R/indvol.R
@@ -66,7 +66,7 @@ indvol <- function(x, mens="plot", vol=F, myeq=NULL, veg=NULL, f=NULL, circ=F) {
       plan<-data.frame(p,row,s,h,d,volume, stringsAsFactors = F)
       
     }else{
-    plan<-data.frame(p,row,s,h,d, stringsAsFactors = F)
+      plan<-data.frame(p,row,s,h,d, stringsAsFactors = F)
     }
     
   }
@@ -147,7 +147,7 @@ indvol <- function(x, mens="plot", vol=F, myeq=NULL, veg=NULL, f=NULL, circ=F) {
       plan<-data.frame(e,p,row,s,h,d,volume, stringsAsFactors = F)
       
     }else{
-    plan<-data.frame(e,p,row,s,h,d, stringsAsFactors = F)
+      plan<-data.frame(e,p,row,s,h,d, stringsAsFactors = F)
     }
   }
   
@@ -185,7 +185,7 @@ indvol <- function(x, mens="plot", vol=F, myeq=NULL, veg=NULL, f=NULL, circ=F) {
     h<- as.numeric(result2[!(result2==-Inf)])
     row<- as.numeric(rownames(result2)[!(result2==-Inf)])
     
-  
+    
     #ESPECIES
     x[,2]<-as.character(x[,2])
     
@@ -200,18 +200,18 @@ indvol <- function(x, mens="plot", vol=F, myeq=NULL, veg=NULL, f=NULL, circ=F) {
     #VOLUME
     if(vol==T){
       
-    resultvol<-as.numeric()
-    for(i in x[,1]){
-      resultvol[i]<-sum(subset(x[,5], x[,1]==i))
-    }
-    resultvol<-as.data.frame(resultvol)
-    
-    volume<- resultvol[!(is.na(resultvol))]
-    
-    plan<-data.frame(row,s,h,d,volume, stringsAsFactors = F)
-    
+      resultvol<-as.numeric()
+      for(i in x[,1]){
+        resultvol[i]<-sum(subset(x[,5], x[,1]==i))
+      }
+      resultvol<-as.data.frame(resultvol)
+      
+      volume<- resultvol[!(is.na(resultvol))]
+      
+      plan<-data.frame(row,s,h,d,volume, stringsAsFactors = F)
+      
     }else{
-    plan<-data.frame(row,s,h,d, stringsAsFactors = F)
+      plan<-data.frame(row,s,h,d, stringsAsFactors = F)
     }
   }
   
@@ -230,73 +230,77 @@ indvol <- function(x, mens="plot", vol=F, myeq=NULL, veg=NULL, f=NULL, circ=F) {
     plan<-x
   }
   
-
+  
   if(vol==F){
-  #Para myeq:
-  
-  if(!(is.null(myeq))){
-  
-  plan$`Volume (m3)` <- eval(substitute(myeq), envir=plan); plan
-  }
-  
-  #Para fator de forma (hf):
-  
-  if(!(is.null(f))){
-    plan$`Volume (m3)`<-(pi*d^2/40000)*h*f
-  }
-  
-  #Para veg:
-  if(!is.null(veg)){
+    #Para myeq:
     
-    if(veg=="cerrado"){
-      plan$`Volume (m3)`<-0.000065661*d^(2.475293)* h^(0.300022)
-    }
-    if(veg=="cer_regen"){
-      plan$`Volume (m3)`<-0.000058468*d^(2.160042)* h^(0.791208)
+    if(!(is.null(myeq))){
+      
+      d<-plan$d
+      h<-plan$h
+      
+      plan$`Volume (m3)` <- eval(parse(text = as.character(myeq)))
+      
     }
     
-    if(veg=="campo_cer"){
-      plan$`Volume (m3)`<-0.000024059*d^(2.506122)* h^(0.929214)
+    #Para fator de forma (hf):
+    
+    if(!(is.null(f))){
+      plan$`Volume (m3)`<-(pi*d^2/40000)*h*f
     }
     
-    if(veg=="cerradao"){
-      plan$`V (m3)`<-0.000094001*d^(1.830398)* h^(0.960913)
+    #Para veg:
+    if(!is.null(veg)){
+      
+      if(veg=="cerrado"){
+        plan$`Volume (m3)`<-0.000065661*d^(2.475293)* h^(0.300022)
+      }
+      if(veg=="cer_regen"){
+        plan$`Volume (m3)`<-0.000058468*d^(2.160042)* h^(0.791208)
+      }
+      
+      if(veg=="campo_cer"){
+        plan$`Volume (m3)`<-0.000024059*d^(2.506122)* h^(0.929214)
+      }
+      
+      if(veg=="cerradao"){
+        plan$`V (m3)`<-0.000094001*d^(1.830398)* h^(0.960913)
+      }
+      
+      if(veg=="mata_pri"){
+        plan$`Volume (m3)`<-0.00024502*d^(2.265786)* h^(0.150001)
+      }
+      
+      if(veg=="mata_sec"){
+        plan$`Volume (m3)`<-0.000074230*d^(1.707348)* h^(1.16873)
+      }
+      
+      if(veg=="mata_ciliar"){
+        plan$`Volume (m3)`<-0.000065607*d^(2.084676)* h^(0.752177)
+      }
+      
+      if(veg=="mata_seca"){
+        plan$`Volume (m3)`<-0.000074924*d^(1.818557)* h^(1.061157)
+      }
+      
+      if(veg=="trans_cipo"){
+        plan$`Volume (m3)`<-0.0000065231*d^(1.64498)* h^(2.234673)
+      }
+      
+      if(veg=="trans_jaiba"){
+        plan$`Volume (m3)`<-0.000057947*d^(1.911894)* h^(1.0751)
+      }
+      
+      if(veg=="caat_arborea"){
+        plan$`Volume (m3)`<-0.0000408657*d^(2.235528)* h^(0.823993)
+      }
+      
+      if(veg=="caat_arbustiva"){
+        plan$`Volume (m3)`<-0.000075999*d^(2.016671)* h^(0.761177)
+      }
+      
     }
     
-    if(veg=="mata_pri"){
-      plan$`Volume (m3)`<-0.00024502*d^(2.265786)* h^(0.150001)
-    }
-    
-    if(veg=="mata_sec"){
-      plan$`Volume (m3)`<-0.000074230*d^(1.707348)* h^(1.16873)
-    }
-    
-    if(veg=="mata_ciliar"){
-      plan$`Volume (m3)`<-0.000065607*d^(2.084676)* h^(0.752177)
-    }
-    
-    if(veg=="mata_seca"){
-      plan$`Volume (m3)`<-0.000074924*d^(1.818557)* h^(1.061157)
-    }
-    
-    if(veg=="trans_cipo"){
-      plan$`Volume (m3)`<-0.0000065231*d^(1.64498)* h^(2.234673)
-    }
-    
-    if(veg=="trans_jaiba"){
-      plan$`Volume (m3)`<-0.000057947*d^(1.911894)* h^(1.0751)
-    }
-    
-    if(veg=="caat_arborea"){
-      plan$`Volume (m3)`<-0.0000408657*d^(2.235528)* h^(0.823993)
-    }
-    
-    if(veg=="caat_arbustiva"){
-      plan$`Volume (m3)`<-0.000075999*d^(2.016671)* h^(0.761177)
-    }
-    
-  }
-  
   }
   
   
@@ -312,7 +316,7 @@ indvol <- function(x, mens="plot", vol=F, myeq=NULL, veg=NULL, f=NULL, circ=F) {
   if(mens=="strata"){
     colnames(plan)[6]<-colnames(x)[6]
   }
-   
-   return(plan)
-
+  
+  return(plan)
+  
 }
